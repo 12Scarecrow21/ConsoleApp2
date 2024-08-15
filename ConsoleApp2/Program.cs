67cb22bee9dce4616cea13c4e5684c2767682312
Your Name <you@example.com>
2024-08-15 11:48:29 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main()
+        {
+            List<Shape> shapes = new List<Shape>();
+
+            while (true)
+            {
+                Console.WriteLine("Выберите фигуру: 1-Прямоугольник, 2-Треугольник, 3-Круг, 0-Выход");
+                int choice = int.Parse(Console.ReadLine());
+
+                if (choice == 0) break;
+
+                Shape shape;
+
+                switch (choice)
+                {
+                    case 1:
+                        Console.WriteLine("1 - Введите ширину и высоту, 2 - Введите координаты противоположных углов");
+                        int rectChoice = int.Parse(Console.ReadLine());
+
+                        if (rectChoice == 1)
+                        {
+                            Console.Write("Введите ширину: ");
+                            double width = double.Parse(Console.ReadLine());
+                            Console.Write("Введите высоту: ");
+                            double height = double.Parse(Console.ReadLine());
+                            shape = new Rectangle(width, height);
+                        }
+                        else
+                        {
+                            Console.Write("Введите координаты первой точки (x1 y1): ");
+                            double x1 = double.Parse(Console.ReadLine());
+                            double y1 = double.Parse(Console.ReadLine());
+                            Console.Write("Введите координаты второй точки (x2 y2): ");
+                            double x2 = double.Parse(Console.ReadLine());
+                            double y2 = double.Parse(Console.ReadLine());
+                            shape = new Rectangle(x1, y1, x2, y2);
+                        }
+                        break;
+
+                    case 2:
+                        Console.WriteLine("1 - Введите длины сторон, 2 - Введите координаты вершин");
+                        int triChoice = int.Parse(Console.ReadLine());
+
+                        if (triChoice == 1)
+                        {
+                            Console.Write("Введите сторону A: ");
+                            double sideA = double.Parse(Console.ReadLine());
+                            Console.Write("Введите сторону B: ");
+                            double sideB = double.Parse(Console.ReadLine());
+                            Console.Write("Введите сторону C: ");
+                            double sideC = double.Parse(Console.ReadLine());
+                            shape = new Triangle(sideA, sideB, sideC);
+                        }
+                        else
+                        {
+                            Console.Write("Введите координаты первой точки (x1 y1): ");
+                            double x1 = double.Parse(Console.ReadLine());
+                            double y1 = double.Parse(Console.ReadLine());
+                            Console.Write("Введите координаты второй точки (x2 y2): ");
+                            double x2 = double.Parse(Console.ReadLine());
+                            double y2 = double.Parse(Console.ReadLine());
+                            Console.Write("Введите координаты третьей точки (x3 y3): ");
+                            double x3 = double.Parse(Console.ReadLine());
+                            double y3 = double.Parse(Console.ReadLine());
+                            shape = new Triangle(x1, y1, x2, y2, x3, y3);
+                        }
+                        break;
+
+                    case 3:
+                        Console.WriteLine("1 - Введите радиус, 2 - Введите координаты центра и точки на окружности");
+                        int circChoice = int.Parse(Console.ReadLine());
+
+                        if (circChoice == 1)
+                        {
+                            Console.Write("Введите радиус: ");
+                            double radius = double.Parse(Console.ReadLine());
+                            shape = new Circle(radius);
+                        }
+                        else
+                        {
+                            Console.Write("Введите координаты центра (x y): ");
+                            double centerX = double.Parse(Console.ReadLine());
+                            double centerY = double.Parse(Console.ReadLine());
+                            Console.Write("Введите координаты точки на окружности (x y): ");
+                            double pointX = double.Parse(Console.ReadLine());
+                            double pointY = double.Parse(Console.ReadLine());
+                            shape = new Circle(centerX, centerY, pointX, pointY);
+                        }
+                        break;
+
+                    default:
+                        Console.WriteLine("Неверный выбор.");
+                        continue;
+                }
+
+                shapes.Add(shape);
+
+                Console.WriteLine($"Площадь: {shape.Area()}");
+                Console.WriteLine($"Периметр: {shape.Perimeter()}");
+
+                if (shape is Rectangle rect)
+                {
+                    Console.WriteLine(rect.Square() ? "Прямоугольник является квадратом." : "Прямоугольник не является квадратом.");
+                }
+                else if (shape is Triangle tri)
+                {
+                    Console.WriteLine(tri.Equilateral() ? "Треугольник является равносторонним." : "Треугольник не является равносторонним.");
+                }
+            }
+            double totalArea = 0;
+            foreach (var shape in shapes)
+            {
+                totalArea += shape.Area();
+            }
+            Console.WriteLine($"Суммарная площадь всех фигур: {totalArea}");
+        }
+    }
+}
---
+++ ConsoleApp2/Shape and Figure.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+     abstract class Shape
+     {
+        public abstract double Area();
+        public abstract double Perimeter();
+    }
+    class Rectangle : Shape
+    {
+        public double Width { get; set; }
+        public double Height { get; set; }
+
+
+        public Rectangle(double width, double height)
+        {
+            Width = width;
+            Height = height;
+        }
+        public Rectangle(double x1, double y1, double x2, double y2)
+        {
+            Width = Math.Abs(x2 - x1);
+            Height = Math.Abs(y2 - y1);
+        }
+
+        public override double Area()
+        {
+            return Width*Height;
+        }
+        public override double Perimeter()
+        {
+            return 2*(Height+Width);
+        }
+        public bool Square() => Height == Width; 
+    }
+    class Triangle : Shape
+    {
+        public double SideA { get; set; }
+        public double SideB { get; set; }
+        public double SideC { get; set; }
+
+        public Triangle(double sidea, double sideb, double sidec) 
+        {
+            SideA = sidea;
+            SideB = sideb;
+            SideC = sidec;
+        }
+        public Triangle(double x1, double y1, double x2, double y2, double x3, double y3)
+        {
+            SideA = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+            SideB = Math.Sqrt(Math.Pow(x3 - x2, 2) + Math.Pow(y3 - y2, 2));
+            SideC = Math.Sqrt(Math.Pow(x1 - x3, 2) + Math.Pow(y1 - y3, 2));
+        }
+        public override double Area()
+        {
+            double p = (SideA + SideB + SideC) / 2;
+            return Math.Sqrt(p * (p - SideA) * (p - SideB) * (p - SideC));
+        }
+        public override double Perimeter()
+        {
+            return  SideA + SideB + SideC;
+        }
+        public bool Equilateral() => SideA == SideB && SideB == SideC;
+    }
+    class Circle : Shape
+    {
+        public double Radius{get; set;}
+        public Circle(double radius)
+        {
+            Radius = radius;
+        }
+        public Circle(double centerX, double centerY, double pointX, double pointY)// pointX/pointY точки на окружности 
+        {
+            Radius = Math.Sqrt(Math.Pow(pointX - centerX, 2) + Math.Pow(pointY - centerY, 2));        
+        }
+        public override double Area() 
+        {
+            return Math.PI * Math.Pow(Radius, 2);
+        }
+        public override double Perimeter()
+        {
+            return 2 * Math.PI * Radius;
+        }
+    }
+}
